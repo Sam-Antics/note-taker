d42ca3ce050180744d41803290d5627af5ebdf72
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-07-18 13:43:36 -0500

    now serves notes.html

--- server.js
+++ server.js
@@ -67,6 +67,9 @@ app.post('/api/notes', (req, res) => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 // LISTEN request for the server 
 app.listen(PORT, () => {
   console.log(`API server now on ${PORT}.`);
