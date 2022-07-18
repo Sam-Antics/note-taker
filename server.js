dada0de0e5ef8ebaa23f1aa0a85c153a297f98f3
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-07-17 19:33:02 -0500

    added Heroku

--- server.js
+++ server.js
@@ -11,11 +11,10 @@ const { notes } = require('./db/db.json');
 
 app.get('/api/notes', (req, res) => {
   let results = notes;
-//   if (req.query) {
-//     results = filterByQuery(req.query, results);
-//   }
+  
   res.json(results);
 });
+
 app.listen(3001, () => {
   console.log(`API server now on port 3001`);
 })
