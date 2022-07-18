a0b29bbc5b1676bc8145cf9411db8ce50e30ac67
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-07-17 21:46:36 -0500

    POSTs now auto-generate an ID for each note

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.get('/api/notes', (req, res) => {
 });
 // POST request api call
 app.post('/api/notes', (req, res) => {
+  // setting an auto-generated ID based on the length property so that notes can be 
+  // distinguised on the server (change later when dealing with DELETE requests)
+  req.body.id = notes.length.toString();
+
   res.json(req.body);
 });
 // LISTEN request for the server 
